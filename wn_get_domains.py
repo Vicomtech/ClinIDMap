253e79732cb9db0442e5f4f6af3d201f445aae5e
ezotova <ezotova@vicomtech.org>
2022-10-17 11:40:50 +0200

    Wordnet to domains

--- wn_get_domains.py
+++ wn_get_domains.py
@@ -7,11 +7,10 @@ df_wikidata = pd.read_csv(os.path.join('database', 'query_wikidata_identificador
 
 df_wikidata = df_wikidata
 
-df_domains = pd.read_csv('wn30_domains.tsv', sep='\t', dtype='str')
+df_domains = pd.read_csv('_wn30_domains.tsv', sep='\t', dtype='str')
 
-print(df_wikidata.head())
-print()
-print(df_domains.head())
+# print(df_wikidata.head())
+# print(df_domains.head())
 
 wn30_list = df_wikidata.wordnet30_id.to_list()
 
@@ -22,8 +21,6 @@ for wn in tqdm.tqdm(wn30_list):
     df_wn_dom['coeff'] = df_wn_dom['coeff'].astype(float).map('{:,.6f}'.format)
     if len(df_wn_dom) > 0: 
         df_wn_dom_sorted = df_wn_dom.sort_values('coeff', ascending=False)
-        # print(df_wn_dom_sorted.head())
-        # print()
         domain = df_wn_dom_sorted.domain.to_list()[0]
         coeff = df_wn_dom_sorted.coeff.to_list()[0]
     else: 
@@ -31,11 +28,8 @@ for wn in tqdm.tqdm(wn30_list):
         coeff = 'no_coeff'
     domains.append(domain)
     coeffs.append(coeff)
-    # print(df_wn_dom.head(20))
 
 df_wikidata['domain'] = domains
 df_wikidata['coeff'] = coeffs
-
-# print(df_wikidata.head(20))
 
 df_wikidata.to_csv(os.path.join('database', 'query_wikidata_identificador_de_synset_de_WordNet_3_1_wn_3_0_domains.tsv'), sep='\t', index=False)
